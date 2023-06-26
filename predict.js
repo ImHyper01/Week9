c5f2abd3eda0c30541e3060d8ad04dad8ad0d7be
Ferdi <57798860+ImHyper01@users.noreply.github.com>
2023-06-26 14:22:11 +0200

    predictie werkt + Style changes

--- predict.js
+++ predict.js
@@ -1,6 +1,6 @@
 const predictButton = document.getElementById('predictBtn').addEventListener("click", function() {makePrediction()});
 const yearsExperienceInput = document.getElementById('YearsExperience');
-
+const resultField =document.getElementById('result');
 
 const nn = ml5.neuralNetwork({ task: 'regression', debug: true })
 nn.load('./model/model.json', modelLoaded)
@@ -11,16 +11,19 @@ function modelLoaded() {
 
 async function makePrediction() {
     const predictionValues = {
-        yearsExperience: parseInt(yearsExperienceInput.value),
+        YearsExperience: parseInt(yearsExperienceInput.value),
     }
 
     const results = await nn.predict(predictionValues);
     console.log(`Het salaris is ${results[0].Salary}`)
 
-    currencyTransformer(results[0].Sakary);
+    currencyTransformer(results[0].Salary);
+
 }
 
 function currencyTransformer(result) {
     const fmt = new Intl.NumberFormat('nl-NL', { style: 'currency', currency: 'EUR' });
     resultField.innerHTML = "Het salaris is " + fmt.format(result);
 }
+
+
